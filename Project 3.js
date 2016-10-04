df0173464c5eb41e860c1f0f1a9117e5c762b47f
chris <christopher.malinosky@gmail.com>
2016-10-04 09:04:49 -0400

    Adde ability to display piece

--- Project 3.js
+++ Project 3.js
@@ -31,8 +31,11 @@ var modelView;
 var aspect;
 
 var piece = [];
-piece = piece.concat(drawShape3D(0, 0, 0, 1, 10, 2*Math.PI, 0));
-piece = piece.concat(drawShape3D(0, 0, 0, 1, 11, 2*Math.PI, 0));
+var pieceSides = 20;
+piece.push(vec4(0,5,0,1));
+piece = piece.concat(drawShape3D(0, 5, 0, 0.5, pieceSides, 2*Math.PI, 0));
+piece.push(vec4(0,4.5,0,1));
+piece = piece.concat(drawShape3D(0, 4.5, 0, 0.5, pieceSides, 2*Math.PI, 0));
 
 window.onload = function init()
 {
@@ -59,14 +62,14 @@ window.onload = function init()
 	let triangleHeight = triangleWidth*5;
 	let triangleZ = boardHeight + 0.01;
 
-	//Add verticies for top triangles
+	//Add vertices for top triangles
 	vertices.push(vec4(triangleWidth,triangleZ,triangleWidth,1.0));
 	for(let i = 0; i<12; i++){
 		vertices.push(vec4((i+2)*triangleWidth,triangleZ,triangleWidth,1.0));
 		vertices.push(vec4((i+1.5)*triangleWidth,triangleZ,triangleWidth + triangleHeight,1.0));
 	}
 
-	//Add verticies for bottom triangles
+	//Add vertices for bottom triangles
 	vertices.push(vec4(triangleWidth,triangleZ,boardSideLength - triangleWidth,1.0));
 	for(let i = 0; i<12; i++){
 		vertices.push(vec4((i+2)*triangleWidth,triangleZ,boardSideLength - triangleWidth,1.0));
@@ -75,11 +78,14 @@ window.onload = function init()
 
 	let backgroundZ = boardHeight + 0.005;
 
-	//Add verticies for game board background
+	//Add vertices for game board background
 	vertices.push(vec4(triangleWidth,backgroundZ,triangleWidth,1.0));
 	vertices.push(vec4(boardSideLength - triangleWidth,backgroundZ,triangleWidth,1.0));
 	vertices.push(vec4(triangleWidth,backgroundZ,boardSideLength - triangleWidth,1.0));
 	vertices.push(vec4(boardSideLength - triangleWidth,backgroundZ,boardSideLength - triangleWidth,1.0));
+
+	//Add vertices for piece
+	vertices = vertices.concat(piece);
 
 	 colors = [
 	 	colorVecFromRGB(205,133,63), // brown
@@ -87,9 +93,6 @@ window.onload = function init()
 	 	vec4(0.0, 0.0, 0.0, 1.0),  // black
 	    vec4(1.0, 0.0, 0.0, 1.0)  // red
 	];
-
-	//Add verticies for piece
-	//verticies.push(piece);
 
 	// Load indices to represent the triangles that will draw each face
 	
@@ -131,6 +134,55 @@ window.onload = function init()
 	indices.push(60);
 	indices.push(61);
 	indices.push(59);
+
+	//Add indexes for a game piece
+	let pieceSize = piece.length;
+	let startingIndex = 62;
+
+	//Top
+	for(let i = 0; i < pieceSize/2 - 1; i++){
+		indices.push(startingIndex);
+		indices.push(startingIndex + i + 1);
+		if(i === pieceSize/2 - 2){
+			indices.push(startingIndex + 1);
+		} else {
+			indices.push(startingIndex + i + 2);
+		}
+	}
+
+	//Sides
+	startingIndex += 1;
+	for(let i = 0; i < pieceSize/2 - 1; i++){
+		indices.push(startingIndex + i);
+		indices.push(startingIndex + i + pieceSize/2);
+		if(i === pieceSize/2 - 2){
+			indices.push(startingIndex + pieceSize/2);
+		} else {
+			indices.push(startingIndex + i + pieceSize/2 + 1);
+		}
+
+		indices.push(startingIndex + i);
+		if(i === pieceSize/2 - 2){
+			indices.push(startingIndex + pieceSize/2);
+			indices.push(startingIndex);
+		} else {
+			indices.push(startingIndex + i + pieceSize/2 + 1);
+			indices.push(startingIndex + i + 1);
+		}
+	}
+	startingIndex -= 1;
+
+	//Bottom
+	startingIndex += piece.length/2;
+	for(let i = 0; i < pieceSize/2 - 1; i++){
+		indices.push(startingIndex);
+		if(i === pieceSize/2 - 2){
+			indices.push(startingIndex + 1);
+		} else {
+			indices.push(startingIndex + i + 2);
+		}
+		indices.push(startingIndex + i + 1);
+	}
 	
 	theta[0] = 0.0;
 	theta[1] = 0.0;
@@ -143,7 +195,7 @@ window.onload = function init()
 	aspect = canvas.width / canvas.height;
     gl.clearColor( 0.7, 0.7, 0.7, 1.0 );
 	gl.enable(gl.DEPTH_TEST);
-	//projection = ortho (windowMin, windowMax, windowMin, windowMax, windowMin, windowMax+boardSideLength);
+	// projection = ortho (windowMin, windowMax, windowMin, windowMax, windowMin, windowMax+boardSideLength);
 	// Register event listeners for the buttons
 	
 	var a=document.getElementById ("XButton");
@@ -152,6 +204,8 @@ window.onload = function init()
 	b.addEventListener ("click", function () { axis = yAxis; });
 	var c=document.getElementById ("ZButton");
 	c.addEventListener ("click", function () { axis = zAxis; });
+	var e=document.getElementById ("Pause");
+	e.addEventListener ("click", function () { axis = null; });
 	var d=document.getElementById ("Reset");
 	d.addEventListener ("click", function () { theta = [0.0, 0.0, 0.0]; axis = xAxis });
 
@@ -221,22 +275,47 @@ function render()
 	modelView = mult(looking, mult(tz2, mult (rotation, tz1)));
 	gl.uniformMatrix4fv (modelViewLoc, false, flatten(modelView));
 	gl.uniformMatrix4fv (projectionLoc, false, flatten(projection));
+
+	//Draw board
+	let currentBufferIndex = 0;
+	let tempBufferIndex = 0;
 	for (var i=0; i<6; i++) {
 		gl.uniform4fv (colorLoc, colors[0]);
 		gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_BYTE, 6*i );
+		tempBufferIndex += 6;
 	}
+	currentBufferIndex += tempBufferIndex;
+	tempBufferIndex = 0;
+
+	//Draw Triangles
 	for (var i=0; i<24; i++){
 		if((i/12) < 1){
 			gl.uniform4fv (colorLoc, colors[(i%2) + 2]);
 		} else {
 			gl.uniform4fv (colorLoc, colors[3 -(i%2)]);
 		}
-		gl.drawElements( gl.TRIANGLES, 3, gl.UNSIGNED_BYTE, 36 + 3*i );
+		gl.drawElements( gl.TRIANGLES, 3, gl.UNSIGNED_BYTE, currentBufferIndex + 3*i );
+		tempBufferIndex += 3;
 	}
+	currentBufferIndex += tempBufferIndex;
+	tempBufferIndex = 0;
 
+	//Draw background
 	gl.uniform4fv (colorLoc, colors[1]);
-	gl.drawElements( gl.TRIANGLES, 3, gl.UNSIGNED_BYTE, 108);
-	gl.drawElements( gl.TRIANGLES, 3, gl.UNSIGNED_BYTE, 111);
+	gl.drawElements( gl.TRIANGLES, 3, gl.UNSIGNED_BYTE, currentBufferIndex);
+	currentBufferIndex += 3;
+	gl.drawElements( gl.TRIANGLES, 3, gl.UNSIGNED_BYTE, currentBufferIndex);
+	currentBufferIndex += 3;
+
+	//Draw piece
+	gl.uniform4fv (colorLoc, colors[2]);
+	for (let i = 0; i < piece.length*2 - 4; i++){
+		gl.drawElements( gl.TRIANGLES, 3, gl.UNSIGNED_BYTE, currentBufferIndex + i*3 );
+		tempBufferIndex += 3;
+	}
+	currentBufferIndex += tempBufferIndex;
+	tempBufferIndex = 0;
+
 	requestAnimFrame (render);
 };
 
@@ -248,8 +327,7 @@ function drawShape3D(x, y, z, length, sides, fullAngle, startAngle) {
     let shape = [];
     let disp = fullAngle / sides;
     for(let i = 0; i < sides; i++){
-        shape.push(vec4(x + Math.cos(startAngle+disp*i)*length, y + Math.sin(startAngle+disp*i)*length, z, 1.0));
-        shape.push(vec4(x + Math.cos(startAngle+disp*(i+1))*length, y + Math.sin(startAngle+disp*(i+1))*length, z, 1.0));
+        shape.push(vec4(x + Math.cos(startAngle+disp*i)*length, y, z + Math.sin(startAngle+disp*i)*length, 1.0));
     }
     return shape;
 }
